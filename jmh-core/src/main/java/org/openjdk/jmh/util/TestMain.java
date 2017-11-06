2fe2b83ddd0fe0121e512bf0d9ef85b2f31c9f19
zhangliang <zhangliang@reyun.com>
2017-11-06 17:44:45 +0800

    初始化

---
+++ jmh-core/src/main/java/org/openjdk/jmh/util/TestMain.java
@@ -0,0 +1,18 @@
+package org.openjdk.jmh.util;
+
+/**
+ * description:
+ *
+ * @author nolan
+ * @date 17/10/2017
+ */
+public class TestMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("123123");
+        System.out.println("123123ddd");
+
+    }
+
+}
